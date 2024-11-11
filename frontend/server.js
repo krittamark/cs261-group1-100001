dd66d8957743edfffb12bf3c8acc091ceffcff80
Santuch Lapsuwannawong - 6609612236 <santuch.lap@dome.tu.ac.th>
2024-11-11 15:32:04 +0700

    feat: run on docker container

--- frontend/server.js
+++ frontend/server.js
@@ -1,27 +1,27 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParserErrorHandler = require('express-body-parser-error-handler');
-const { urlencoded, json } = require('body-parser');
-const path = require('path');
-const cors = require('cors');
+const express = require("express");
+const bodyParserErrorHandler = require("express-body-parser-error-handler");
+const { urlencoded, json } = require("body-parser");
+const path = require("path");
+const cors = require("cors");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 app.use(urlencoded({ extended: true }));
 app.use(json());
 app.use(bodyParserErrorHandler());
-app.use(cors())
+app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: err.message });
+    console.error(err.stack);
+    res.status(500).json({ message: err.message });
 });
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+    console.log(`App listening on port ${PORT}`);
 });
